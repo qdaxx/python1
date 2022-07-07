42b35e1805222c553f3769b10324b95dc8495b1d
qdaxx <colomatkin@mail.ru>
2022-07-08 01:55:38 +0300

    2urok

---
+++ 2urok.py
@@ -0,0 +1,31 @@
+print(2 is 2)
+
+a=5
+print(0<a<3)
+
+import math
+b=6.25
+print(b)
+print(math.trunc(b))
+print(math.floor(b))
+print(math.ceil(b))
+
+print(math.e)
+print(math.pi)
+print(math.sin(math.pi / 2))
+print(math.sqrt(4) * 2)
+
+string1=input()
+print(id(string1))
+
+string = "100\n" \
+        "200"
+print(string)
+
+string4 = """Всем привет.
+Меня зовут Иван
+123141
+"""
+print(string4)
+
+print('he' , 'llo', sep='')
